1512d885c5f5615d4483fb0a5870263784f372a8
Paul M. Jones <pmjones88@gmail.com>
2015-05-06 15:06:48 -0500

    use Boot not Factory

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
+namespace Radar\Adr;
+
 require '../vendor/autoload.php';
 
-$env = dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env';
-$adr = (new \Radar\Adr\Factory($env))->newInstance([
-    'Example\Config',
+$boot = new Boot(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env');
+$adr = $boot([
+    'Example\Config'
 ]);
 
 $adr->get('Example\Hello', '/{name}?', 'Example\Hello\Domain')
     ->defaults(['name' => 'world']);
-
-$adr->get('Example\Foo', '/foo/{bar}', 'Example\Foo\Domain');
 
 $adr();
