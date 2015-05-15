cfd6060b5b6a1bbe320ef0ca4e5a9a042ceeaa5c
Paul M. Jones <pmjones88@gmail.com>
2015-05-15 11:28:23 -0500

    get rid of some of the 'hipster' __invoke()s; blame @shochdoerfer

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ namespace Radar\Adr;
 require '../vendor/autoload.php';
 
 $boot = new Boot(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env');
-$adr = $boot();
+$adr = $boot->adr();
 
 $adr->get('Hello', '/{name}?', function (array $input) {
         $payload = new \Aura\Payload\Payload();
@@ -16,4 +16,4 @@ $adr->get('Hello', '/{name}?', function (array $input) {
     })
     ->defaults(['name' => 'world']);
 
-$adr();
+$adr->run();
