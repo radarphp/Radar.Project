73cc6b732dd648b5fbaeeb6012b33a23ca597a97
Paul M. Jones <pmjones88@gmail.com>
2015-05-24 11:35:53 -0500

    use Diactoros, and update handler stack

--- web/index.php
+++ web/index.php
@@ -1,11 +1,21 @@
 <?php
 namespace Radar\Adr;
 
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response;
+
 require '../vendor/autoload.php';
 
+// boot
 $boot = new Boot(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env');
 $adr = $boot->adr();
 
+// middleware
+$adr->middle('Radar\Adr\Handler\RoutingHandler');
+$adr->middle('Radar\Adr\Handler\ActionHandler');
+$adr->middle('Radar\Adr\Handler\SendingHandler');
+
+// routing
 $adr->get('Hello', '/{name}?', function (array $input) {
         $payload = new \Aura\Payload\Payload();
         return $payload
@@ -16,4 +26,8 @@ $adr->get('Hello', '/{name}?', function (array $input) {
     })
     ->defaults(['name' => 'world']);
 
-$adr->run();
+// run
+$adr->run(
+    ServerRequestFactory::fromGlobals(),
+    new Response()
+);
