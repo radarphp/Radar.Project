7b1038e4f5dc02ecb7ed62a54882955025d7bb39
Paul M. Jones <pmjones88@gmail.com>
2015-05-28 17:58:08 -0500

    don't override just yet

--- web/index.php
+++ web/index.php
@@ -5,9 +5,6 @@
 require '../vendor/autoload.php';
 $boot = new Radar\Adr\Boot([
     'filepath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env',
-    'toEnv' => true,
-    'toServer' => true,
-    'putenv' => true,
 ]);
 $adr = $boot->adr();
 
